cf738b43e20880d119e68df25a2cb220ba552319
Garima268 <60786500+Garima268@users.noreply.github.com>
2020-02-27 12:46:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import sys
 import os
 #Enter full path to blast executables
-path_blast = "/home/sharat/PycharmProjects/ncbi-blast-2.10.0+/bin"
+path_blast = "/usr/bin/ncbi-blast-2.10.0+/bin"
 #Enter full path to ViennaRNA package directory
-path_vienna = "/home/sharat/PycharmProjects/ViennaRNA-2.4.14"
+path_vienna = "/home/User/Projects/ViennaRNA-2.4.14"
 #Enter full path to Uniprot/nr database directory
-path_db = "/home/sharat/miRMaster/DB/"
-#Enter name of Database alias 
+path_db = "/home/User/miRMaster/DB/"
+#Enter name of Uniprot fasta 
 db_name = "uniprot"
 
